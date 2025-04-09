e01089b87853afc9315cfd399537e4536f89f1f7
jasozh5 <jasonzhao@ucsb.edu>
2025-04-09 16:48:58 -0700

    first commit

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Jason Z.";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "jasozh5";
     }
 
     /**
@@ -40,7 +40,7 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
+        Team team = new Team("s25-09");
         team.addMember("Alice");
         team.addMember("Bob");
         team.addMember("Chris G.");
